9fe19229a3ec50000322c3b4a86f657ef7027de1
Arun Prabhu K <arun.prabhu@capestart.com>
2020-09-19 11:28:03 +0530

    Add CustomError class

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,13 @@
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  constructor(public reason: string = "error connecting to database") {
+    super(reason);
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
