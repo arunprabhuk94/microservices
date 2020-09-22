a599259431cdc7ccb6409183a85901b1b9a4bbee
Arun Prabhu K <arun.prabhu@capestart.com>
2020-09-22 05:35:07 +0530

    Add tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,26 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@arptickets/common";
+
+import { createTicketRouter } from "./routes/new";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(createTicketRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
