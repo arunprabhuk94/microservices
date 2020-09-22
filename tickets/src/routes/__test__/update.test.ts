782ba08c22b718aa496d12f16c09816c310817f2
Arun Prabhu K <arun.prabhu@capestart.com>
2020-09-22 11:19:06 +0530

    Complete tickets service

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -32,6 +32,47 @@ it("returns a 401 if the user does not own the ticket", async () => {
     .expect(401);
 });
 
-it("returns a 400 if the user provides an invalid title or price", async () => {});
+it("returns a 400 if the user provides an invalid title or price", async () => {
+  const cookie = global.signin();
 
-it("updates the ticket provided valid inputs", async () => {});
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({ title: "ticky", price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "", price: 20 })
+    .expect(400);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "tickyTick", price: -10 })
+    .expect(400);
+});
+
+it("updates the ticket provided valid inputs", async () => {
+  const cookie = global.signin();
+  const updatedTicket = {
+    title: "tickyTick",
+    price: 30,
+  };
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({ title: "ticky", price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send(updatedTicket)
+    .expect(200);
+  const ticketRespone = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+  expect(ticketRespone.body.title).toBe(updatedTicket.title);
+  expect(ticketRespone.body.price).toBe(updatedTicket.price);
+});
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -15,13 +15,27 @@ const router = express.Router();
 router.put(
   "/api/tickets/:id",
   requireAuth,
+  [
+    body("title").notEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const { id } = req.params;
+    const { title, price } = req.body;
     const ticket = await Ticket.findById(id);
 
     if (!ticket) {
       throw new NotFoundError();
     }
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    ticket.set({ title, price });
+    await ticket.save();
+
     res.send(ticket);
   }
 );
