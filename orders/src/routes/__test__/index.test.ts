3d9d30d85fd307d2907763574c1388d400800c51
Arun Prabhu K <arun.prabhu@capestart.com>
2020-09-26 00:29:05 +0530

    Add handler and test for new and index of orders

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+  return ticket;
+};
+
+it("fetches orders for a particular user", async () => {
+  //create one order as User 1
+  const ticketOne = await buildTicket();
+
+  //create two orders as User 2
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  //create an order for User 1
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  //create an order for User 2
+  const { body: orderOne } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+  const { body: orderTwo } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  //Make request to get orders for User 2
+  const { body: orders } = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .send()
+    .expect(200);
+
+  expect(orders.length).toBe(2);
+  expect(orders[0].id).toBe(orderOne.id);
+  expect(orders[1].id).toBe(orderTwo.id);
+  //Make sure we only got the orders for User 2
+});
---
+++ orders/src/routes/new.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from "@arptickets/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
+import { NotBeforeError } from "jsonwebtoken";
+
+const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .notEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Ticket Id must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    res.status(201).send(order);
+  }
+);
+
+export { router as newOrderRouter };
